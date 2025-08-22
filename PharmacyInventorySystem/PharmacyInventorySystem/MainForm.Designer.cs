0ef7f2c5e80e2913cd5d1f4aa90aaa00fe4209f7
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-22 03:19:26 +0000

    Added event handlers in the main form file and updated the mainform database file

--- PharmacyInventorySystem/PharmacyInventorySystem/MainForm.Designer.cs
+++ PharmacyInventorySystem/PharmacyInventorySystem/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PharmacyInventorySystem
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,377 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbMedicineDetails = new System.Windows.Forms.GroupBox();
+            this.txtMedicineID = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Price = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMedicineName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbActions = new System.Windows.Forms.GroupBox();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnRecordSale = new System.Windows.Forms.Button();
+            this.btnUpdateStock = new System.Windows.Forms.Button();
+            this.btnAddMedicine = new System.Windows.Forms.Button();
+            this.gbSearch = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gbSales = new System.Windows.Forms.GroupBox();
+            this.txtSaleAmount = new System.Windows.Forms.TextBox();
+            this.txtQuantitySold = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dgvMedicines = new System.Windows.Forms.DataGridView();
+            this.gbMedicineDetails.SuspendLayout();
+            this.gbActions.SuspendLayout();
+            this.gbSearch.SuspendLayout();
+            this.gbSales.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicines)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbMedicineDetails
+            // 
+            this.gbMedicineDetails.Controls.Add(this.txtMedicineID);
+            this.gbMedicineDetails.Controls.Add(this.txtQuantity);
+            this.gbMedicineDetails.Controls.Add(this.label5);
+            this.gbMedicineDetails.Controls.Add(this.label4);
+            this.gbMedicineDetails.Controls.Add(this.Price);
+            this.gbMedicineDetails.Controls.Add(this.txtPrice);
+            this.gbMedicineDetails.Controls.Add(this.txtCategory);
+            this.gbMedicineDetails.Controls.Add(this.label3);
+            this.gbMedicineDetails.Controls.Add(this.label2);
+            this.gbMedicineDetails.Controls.Add(this.txtMedicineName);
+            this.gbMedicineDetails.Controls.Add(this.label1);
+            this.gbMedicineDetails.Location = new System.Drawing.Point(12, 12);
+            this.gbMedicineDetails.Name = "gbMedicineDetails";
+            this.gbMedicineDetails.Size = new System.Drawing.Size(350, 200);
+            this.gbMedicineDetails.TabIndex = 0;
+            this.gbMedicineDetails.TabStop = false;
+            this.gbMedicineDetails.Text = "Medicine Details";
+            // 
+            // txtMedicineID
+            // 
+            this.txtMedicineID.Location = new System.Drawing.Point(120, 145);
+            this.txtMedicineID.Name = "txtMedicineID";
+            this.txtMedicineID.Size = new System.Drawing.Size(200, 22);
+            this.txtMedicineID.TabIndex = 10;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(120, 112);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(200, 22);
+            this.txtQuantity.TabIndex = 9;
+            this.txtQuantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Medicine ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantity";
+            // 
+            // Price
+            // 
+            this.Price.AutoSize = true;
+            this.Price.Location = new System.Drawing.Point(15, 85);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(56, 16);
+            this.Price.TabIndex = 2;
+            this.Price.Text = "Price ($)";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(120, 82);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(200, 22);
+            this.txtPrice.TabIndex = 5;
+            this.txtPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(120, 52);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(200, 22);
+            this.txtCategory.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Category";
+            // 
+            // txtMedicineName
+            // 
+            this.txtMedicineName.Location = new System.Drawing.Point(120, 22);
+            this.txtMedicineName.Name = "txtMedicineName";
+            this.txtMedicineName.Size = new System.Drawing.Size(200, 22);
+            this.txtMedicineName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Medicine Name";
+            // 
+            // gbActions
+            // 
+            this.gbActions.Controls.Add(this.btnViewAll);
+            this.gbActions.Controls.Add(this.btnRecordSale);
+            this.gbActions.Controls.Add(this.btnUpdateStock);
+            this.gbActions.Controls.Add(this.btnAddMedicine);
+            this.gbActions.Location = new System.Drawing.Point(380, 12);
+            this.gbActions.Name = "gbActions";
+            this.gbActions.Size = new System.Drawing.Size(200, 200);
+            this.gbActions.TabIndex = 1;
+            this.gbActions.TabStop = false;
+            this.gbActions.Text = "Actions";
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Location = new System.Drawing.Point(15, 145);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(160, 30);
+            this.btnViewAll.TabIndex = 8;
+            this.btnViewAll.Text = "View All Medicines";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // btnRecordSale
+            // 
+            this.btnRecordSale.Location = new System.Drawing.Point(15, 105);
+            this.btnRecordSale.Name = "btnRecordSale";
+            this.btnRecordSale.Size = new System.Drawing.Size(160, 30);
+            this.btnRecordSale.TabIndex = 7;
+            this.btnRecordSale.Text = "Record Sale";
+            this.btnRecordSale.UseVisualStyleBackColor = true;
+            this.btnRecordSale.Click += new System.EventHandler(this.btnRecordSale_Click);
+            // 
+            // btnUpdateStock
+            // 
+            this.btnUpdateStock.Location = new System.Drawing.Point(15, 65);
+            this.btnUpdateStock.Name = "btnUpdateStock";
+            this.btnUpdateStock.Size = new System.Drawing.Size(160, 30);
+            this.btnUpdateStock.TabIndex = 6;
+            this.btnUpdateStock.Text = "Update Stock";
+            this.btnUpdateStock.UseVisualStyleBackColor = true;
+            this.btnUpdateStock.Click += new System.EventHandler(this.btnUpdateStock_Click);
+            // 
+            // btnAddMedicine
+            // 
+            this.btnAddMedicine.Location = new System.Drawing.Point(15, 25);
+            this.btnAddMedicine.Name = "btnAddMedicine";
+            this.btnAddMedicine.Size = new System.Drawing.Size(160, 30);
+            this.btnAddMedicine.TabIndex = 5;
+            this.btnAddMedicine.Text = "Add Medicine";
+            this.btnAddMedicine.UseVisualStyleBackColor = true;
+            this.btnAddMedicine.Click += new System.EventHandler(this.btnAddMedicine_Click);
+            // 
+            // gbSearch
+            // 
+            this.gbSearch.Controls.Add(this.btnClear);
+            this.gbSearch.Controls.Add(this.btnSearch);
+            this.gbSearch.Controls.Add(this.txtSearchTerm);
+            this.gbSearch.Controls.Add(this.label6);
+            this.gbSearch.Location = new System.Drawing.Point(600, 12);
+            this.gbSearch.Name = "gbSearch";
+            this.gbSearch.Size = new System.Drawing.Size(250, 120);
+            this.gbSearch.TabIndex = 2;
+            this.gbSearch.TabStop = false;
+            this.gbSearch.Text = "Search";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(180, 75);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(60, 27);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(180, 43);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(60, 27);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Location = new System.Drawing.Point(15, 45);
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(160, 22);
+            this.txtSearchTerm.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Search By Name/Category";
+            // 
+            // gbSales
+            // 
+            this.gbSales.Controls.Add(this.txtSaleAmount);
+            this.gbSales.Controls.Add(this.txtQuantitySold);
+            this.gbSales.Controls.Add(this.label8);
+            this.gbSales.Controls.Add(this.label7);
+            this.gbSales.Location = new System.Drawing.Point(12, 220);
+            this.gbSales.Name = "gbSales";
+            this.gbSales.Size = new System.Drawing.Size(350, 80);
+            this.gbSales.TabIndex = 3;
+            this.gbSales.TabStop = false;
+            this.gbSales.Text = "Record Sale";
+            // 
+            // txtSaleAmount
+            // 
+            this.txtSaleAmount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtSaleAmount.Location = new System.Drawing.Point(120, 47);
+            this.txtSaleAmount.Name = "txtSaleAmount";
+            this.txtSaleAmount.ReadOnly = true;
+            this.txtSaleAmount.Size = new System.Drawing.Size(100, 22);
+            this.txtSaleAmount.TabIndex = 3;
+            // 
+            // txtQuantitySold
+            // 
+            this.txtQuantitySold.Location = new System.Drawing.Point(120, 22);
+            this.txtQuantitySold.Name = "txtQuantitySold";
+            this.txtQuantitySold.Size = new System.Drawing.Size(100, 22);
+            this.txtQuantitySold.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 50);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Sale Amount:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Quantity to Sell";
+            // 
+            // dgvMedicines
+            // 
+            this.dgvMedicines.AllowUserToAddRows = false;
+            this.dgvMedicines.AllowUserToDeleteRows = false;
+            this.dgvMedicines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMedicines.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMedicines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedicines.Location = new System.Drawing.Point(12, 320);
+            this.dgvMedicines.Name = "dgvMedicines";
+            this.dgvMedicines.ReadOnly = true;
+            this.dgvMedicines.RowHeadersVisible = false;
+            this.dgvMedicines.RowHeadersWidth = 51;
+            this.dgvMedicines.RowTemplate.Height = 24;
+            this.dgvMedicines.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMedicines.Size = new System.Drawing.Size(950, 300);
+            this.dgvMedicines.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.dgvMedicines);
+            this.Controls.Add(this.gbSales);
+            this.Controls.Add(this.gbSearch);
+            this.Controls.Add(this.gbActions);
+            this.Controls.Add(this.gbMedicineDetails);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pharmacy Management System";
+            this.gbMedicineDetails.ResumeLayout(false);
+            this.gbMedicineDetails.PerformLayout();
+            this.gbActions.ResumeLayout(false);
+            this.gbSearch.ResumeLayout(false);
+            this.gbSearch.PerformLayout();
+            this.gbSales.ResumeLayout(false);
+            this.gbSales.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicines)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbMedicineDetails;
+        private System.Windows.Forms.TextBox txtMedicineName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Price;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMedicineID;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox gbActions;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnRecordSale;
+        private System.Windows.Forms.Button btnUpdateStock;
+        private System.Windows.Forms.Button btnAddMedicine;
+        private System.Windows.Forms.GroupBox gbSearch;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox gbSales;
+        private System.Windows.Forms.TextBox txtSaleAmount;
+        private System.Windows.Forms.TextBox txtQuantitySold;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dgvMedicines;
     }
 }
 
--- PharmacyInventorySystem/PharmacyInventorySystem/MainForm.cs
+++ PharmacyInventorySystem/PharmacyInventorySystem/MainForm.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace PharmacyInventorySystem
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        // event handlers for button clicks
+        // event handler for Add Medicine button
+        private void btnAddMedicine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // event handler for Update Stock button
+        private void btnUpdateStock_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // event handler for Record Sale button
+        private void btnRecordSale_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // event handler for View All button
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // event handler for Search button
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // event handler for Clear button
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
